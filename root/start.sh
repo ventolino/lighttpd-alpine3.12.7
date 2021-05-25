1b85b05fd0612236525f91e955c4cdaa5e7a2e13
ventolino <casferro@gmail.com>
2021-05-25 21:38:44 +0200

    minor

--- root/start.sh
+++ root/start.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
 #Este fichero es para dejar dentro de /root y ejecutarse p.defeto en el container
-
-
-
 #forma de ejecutar: con configuración personalizada y sin detach (ya que vuelca log en fichero, no en consola)
 /usr/sbin/lighttpd -D -f /root/lighttpd.conf
-
